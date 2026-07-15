e34a851a1c78c499f3612624a85704dca2aaeee0
sujacse11 <sujacse11@gmail.com>
2026-07-15 09:41:31 +0530

    Online Invoice Generator

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Invoice-Generator-Day-66-and-67/',
+  base: '/Invoice-Generator-Day-68-Final/',
 })
